aa202b6291259b6e3a415370846a259941850660
Mizan <109397023+Mizan9999@users.noreply.github.com>
2025-06-07 16:07:28 +0600

    day 4

--- day 4/new.py
+++ day 4/new.py
@@ -1,3 +1,21 @@
 import random
-number = random.randint(1,5)
-print(number)
+game = ["rock", "paper", "scissors"]
+pc_select = game[random.randint(0,2)]
+my_select = input("Whats you select? rock, paper, scissors\n")
+
+if my_select == "rock" and pc_select == "scissors":
+    print("You Won the game!")
+elif my_select == "paper" and pc_select == "rock":
+    print("You Won the game!")
+elif my_select == "scissors" and pc_select == "paper":
+    print("You Won the game!")
+elif my_select == "rock" and pc_select == "rock":
+    print("Draw!")
+elif my_select == "paper" and pc_select == "paper":
+    print("Draw")
+elif my_select == "scissors" and pc_select == "scissors":
+    print("draw")
+else:
+    print("you lose the game!")
+
+print(f"\n \n{pc_select}")
