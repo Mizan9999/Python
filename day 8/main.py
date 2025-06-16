fad4faf203babd8f955df912716d7a09e6888219
Mizan <109397023+Mizan9999@users.noreply.github.com>
2025-06-16 19:05:10 +0600

    day 8

--- day 8/main.py
+++ day 8/main.py
@@ -1,47 +1,58 @@
 char = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M','N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-type_operation = input("Encode or Decode: ").lower()
 
-msg = input("message: ")
-decode_char = list(reversed(char))
-def encode(msg):
-    result_list = []
-    n = len(char)  # Calculate once outside loop
+run_code = True
+while run_code:
+
+    type_operation = input("Encode or Decode: ").lower()
+    def encode(msg):
+        result_list = []
+        n = len(char)  # Calculate once outside loop
+        
+        for letter in msg:
+            found = False
+            for i in range(n):
+                if letter == char[i]:
+                    new_index = i + 2
+                    if new_index >= n:
+                        new_index -= n  # Wrap around
+                    result_list.append(char[new_index])
+                    found = True
+                    break
+            if not found:
+                result_list.append(letter)
+        return "".join(result_list)
     
-    for letter in msg:
-        found = False
-        for i in range(n):
-            if letter == char[i]:
-                new_index = i + 2
-                if new_index >= n:
-                    new_index -= n  # Wrap around
-                result_list.append(char[new_index])
-                found = True
-                break
-        if not found:
-            result_list.append(letter)
-    return "".join(result_list)
+    decode_char = list(reversed(char))
 
-def decode(msg):
-    result_list = []
-    n = len(decode_char)
-    
-    for letter in msg:
-        found = False
-        for i in range(n):
-            if letter == decode_char[i]:
-                new_index = i + 2  # Shift forward in reversed list
-                if new_index >= n:
-                    new_index -= n
-                result_list.append(decode_char[new_index])
-                found = True
-                break
-        if not found:
-            result_list.append(letter)
-    return "".join(result_list)
+    def decode(msg):
+        result_list = []
+        n = len(decode_char)
+        
+        for letter in msg:
+            found = False
+            for i in range(n):
+                if letter == decode_char[i]:
+                    new_index = i + 2  # Shift forward in reversed list
+                    if new_index >= n:
+                        new_index -= n
+                    result_list.append(decode_char[new_index])
+                    found = True
+                    break
+            if not found:
+                result_list.append(letter)
+        return "".join(result_list)
 
 
 
-if type_operation == "encode":
-    print(encode(msg))
-elif type_operation == "decode":
-    print(decode(msg))
+    if type_operation == "encode":
+        msg = input("message: ")
+        print(encode(msg))
+    elif type_operation == "decode":
+        msg = input("message: ")
+        print(decode(msg))
+    elif type_operation =="break":
+        run_code = False
+
+    else:
+        print("wrong input")
+        
