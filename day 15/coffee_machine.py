09f0a2e5c598ee58ae93c11c9f5f3bd0b72251e5
Mizan <109397023+Mizan9999@users.noreply.github.com>
2025-06-26 08:53:20 +0600

    day 16

---
+++ day 15/coffee_machine.py
@@ -0,0 +1,22 @@
+coffee_type = int(input("Which Type Coffee You Want? \n1. Espresso  2.Latte 3.Cappuccino\n"))
+store_water = 300
+store_milk = 200
+store_coffee_powder = 100
+money = 0
+if coffee_type == 1:
+    water = 50
+    coffee_powder = 18
+    price = 1.50
+
+elif coffee_type == 2:
+    water = 200
+    coffee_powder = 24
+    milk = 150
+    price = 2.50
+
+elif coffee_type == 3:
+    water = 250
+    coffee_powder = 24
+    milk = 100
+    price = 3.00
+
